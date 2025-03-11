6659d7f095e9b9c921455eb35830cc49df737724
Fir <834772509@qq.com>
2025-03-11 22:57:43 +0800

    add some ver

--- lib/AddEnv.cmd
+++ lib/AddEnv.cmd
@@ -3,7 +3,7 @@ echo [MACRO]AddEnvVar %*
 setlocal enabledelayedexpansion
 set "raw_path=%~1"
 
-set "replace_list=%%X_WOW64%%=X:\Windows\SysWOW64;%%X_SYS%%=X:\Windows\System32;%%X_WIN%%=X:\Windows;%%X%%=X:"
+set "replace_list=%%X_PF%%=X:\Program Files;%%X_PF(x86)%%=Program Files (x86);%%X_WIN%%=X:\Windows;%%X_SYS%%=X:\Windows\System32;%%X_WOW64%%=X:\Windows\SysWOW64;%%X%%=X:"
 for %%m in ("!replace_list:;=" "!") do (
   for /f "tokens=1,2 delims==" %%a in (%%m) do (
     set "raw_path=!raw_path:%%a=%%b!"
